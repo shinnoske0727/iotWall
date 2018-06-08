09d97738afa02269810174e12e33702dd633d951
irie-shinnosuke <irie-shinnosuke@kayac.com>
2018-06-08 11:20:32 +0900

    一旦綺麗にする。音追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,121 +1,25 @@
+import {
+    Howl,
+    Howler
+} from 'howler';
 import '../scss/style.scss';
-const SERVICE_UUID = "713d0000-503e-4c75-ba94-3148f18d941e";
-const RX_CHARACTERISTIC_UUID = "713d0002-503e-4c75-ba94-3148f18d941e";
-const TX_CHARACTERISTIC_UUID = "713d0003-503e-4c75-ba94-3148f18d941e";
+import se from '../sound/wall_se_1.mp3';
+import Bluetooth from './modules/Bluetooth';
 
-// Characteristic
-let txCharacteristic;
-let rxCharacteristic;
 
-let searchButton;
-let readButton;
-let writeButton;
-let loading;
+const sound = new Howl({
+    src: [se]
+});
 
-function init() {
-  searchButton = document.querySelector("#search-button");
-  searchButton.addEventListener("click", searchBLE);
+const searchButton = document.querySelector('#search-button .btn');
 
-  readButton = document.querySelector("#read-button");
-  readButton.addEventListener("click", readValueBLE);
+console.log(searchButton)
+const bluetooth = new Bluetooth({
+    search: searchButton
+})
 
-  writeButton = document.querySelector("#write-button");
-  writeButton.addEventListener("click", writeValueBLE);
+bluetooth.on('near', () => {
+    sound.play();
+})
 
-  loading = document.querySelector("#loading");
-}
-
-// search & connect
-function searchBLE() {
-  // ローディング画面の表示
-  loading.className = "show";
-
-  // acceptAllDevicesの場合optionalServicesが必要みたい
-  navigator.bluetooth.requestDevice({
-    optionalServices:[SERVICE_UUID],
-    acceptAllDevices:true
-  })
-  
-    .then(device => { 
-      console.log("devicename:" + device.name);
-      console.log("id:" + device.id);
-
-      // 選択したデバイスに接続
-      return device.gatt.connect();
-    })
-
-    .then(server => {
-      console.log("success:connect to device");
-
-      // UUIDに合致するサービス(機能)を取得
-      return server.getPrimaryService(SERVICE_UUID);
-    })
-
-    .then(service => {
-      console.log("success:service");
-      // UUIDに合致するキャラクタリスティック(サービスが扱うデータ)を取得
-      // 配列で複数のキャラクタリスティックの取得が可能
-      return Promise.all([
-        service.getCharacteristic(RX_CHARACTERISTIC_UUID),
-        service.getCharacteristic(TX_CHARACTERISTIC_UUID)
-      ]);
-      
-    })
-    .then(characteristic => {
-      console.log("success:txcharacteristic");
-
-      rxCharacteristic = characteristic[0];
-      txCharacteristic = characteristic[1];
-      setInterval(() => {
-        rxCharacteristic.readValue()
-          .then(value => {
-            let message;
-            message = value.buffer;
-            console.log(new Uint8Array(message));
-      });
-      }, 1000)
-      
-
-      console.log("success:connect BLE");      
-      loading.className = "hide";
-    })
-
-    .catch(error => {
-      console.log("Error : " + error);
-
-      // loading非表示
-      loading.className = "hide";
-    });
-}
-
-function readValueBLE() {
-let message;
-
-  try {
-    rxCharacteristic.readValue()
-      .then(value => {
-        message = value.buffer;
-        console.log(new Uint8Array(message));
-        document.getElementById("data-form").value = new TextDecoder("utf-8").decode(message);
-      });
-  }
-  catch (e) {
-    console.log(e);
-  }
-}
-
-function writeValueBLE() {
-  var form_d = document.getElementById("data-form").value;
-
-  var ary_u8 = new Uint8Array( new TextEncoder("utf-8").encode(form_d) );
-  console.log(ary_u8);
-  try {
-    txCharacteristic.writeValue(ary_u8);
-  }
-  catch (e) {
-    console.log(e);
-  }
-}
-
-window.addEventListener("load", init);
 
--- src/js/modules/Bluetooth.js
+++ src/js/modules/Bluetooth.js
@@ -1,91 +1,65 @@
+import EventEmitter from 'events';
+
 const SERVICE_UUID = "713d0000-503e-4c75-ba94-3148f18d941e";
-const TEMPERATURE_CHARACTERISTIC_UUID = "713d0002-503e-4c75-ba94-3148f18d941e";
-const HOGE_CHARACTERISTIC_UUID = "713d0003-503e-4c75-ba94-3148f18d941e";
+const RX_CHARACTERISTIC_UUID = "713d0002-503e-4c75-ba94-3148f18d941e";
+const TX_CHARACTERISTIC_UUID = "713d0003-503e-4c75-ba94-3148f18d941e";
 
-
-export default class Bluetooth {
+export default class Bluetooth extends EventEmitter {
   constructor(opts={}) {
+    super();
+    this._txCharacteristic = null;
+    this._rxCharacteristic = null;
 
-    this.button = opts.button;
-
-    let connectButton;
+    this._searchButton = opts.search;
 
     this.init();
   }
 
   init() {
-    this.button.addEventListener("click", this.connectBLE);
+    this._searchButton.addEventListener("click", this.connectBLE.bind(this));
   }
 
   connectBLE() {
-
+    // acceptAllDevicesの場合optionalServicesが必要みたい
     navigator.bluetooth.requestDevice({
-      filters: [
-        {
-          services: [
-            SERVICE_UUID
-          ]
-        }
-      ]
-    })
-      .then(device => {
-        console.log("デバイスを選択しました。接続します。");
-        console.log("デバイス名 : " + device.name);
-        console.log("ID : " + device.id);
-
-        // 選択したデバイスに接続
-        return device.gatt.connect();
-      })
-      .then(server => {
-        console.log("デバイスへの接続に成功しました。サービスを取得します。");
-
-        // UUIDに合致するサービス(機能)を取得
-        return server.getPrimaryService(SERVICE_UUID);
-      })
-      .then(service => {
-        console.log("サービスの取得に成功しました。キャラクタリスティックを取得します。");
-
-        // UUIDに合致するキャラクタリスティック(サービスが扱うデータ)を取得
-        return Promise.all([
-          service.getCharacteristic(TEMPERATURE_CHARACTERISTIC_UUID),
-          service.getCharacteristic(HOGE_CHARACTERISTIC_UUID)
-        ]);
-      })
-      .then(characteristic => {
-        temperatureCharacteristic = characteristic[1];
-
-        console.log("BLE接続が完了しました。");
-
-        // センサーの値を読み込みます。
-        loadSensorValue();
-
-      })
-      .catch(error => {
-        console.log("Error : " + error);
-      });
-  }
-
-  loadSensorValue() {
-    setInterval(function () {
-      let temperature;
-
-      temperatureCharacteristic.readValue()
-        .then(value => {
-          temperature = value.getUint8(0);
-          temperatureText.innerHTML = temperature;
-
-          console.log("湿度 : " + humidity + "% | 温度 : " + temperature + "度");
-
+            optionalServices: [SERVICE_UUID],
+            acceptAllDevices: true
+        })
+        .then(device => {
+            console.log("devicename:" + device.name);
+            console.log("id:" + device.id);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log("success:connect to device");
+            return server.getPrimaryService(SERVICE_UUID);
+        })
+        .then(service => {
+            console.log("success:service");
+            return Promise.all([
+                service.getCharacteristic(RX_CHARACTERISTIC_UUID),
+                service.getCharacteristic(TX_CHARACTERISTIC_UUID)
+            ]);
+        })
+        .then(characteristic => {
+            console.log("success:txcharacteristic");
+            this._rxCharacteristic = characteristic[0];
+            this._txCharacteristic = characteristic[1];
+            setInterval(() => {
+                this._rxCharacteristic.readValue()
+                    .then(value => {
+                        let message;
+                        message = value.buffer;
+                        console.log(new Uint8Array(message)[0]);
+                        if (new Uint8Array(message)[0] >= 55) {
+                          this.emit('near')
+                        }
+                    });
+            }, 500);
+            console.log("success:connect BLE");
         })
         .catch(error => {
-          console.log("Error : " + error);
+            console.log("Error : " + error);
         });
-
-    }, 1000);
-  }
-
-
-  initListener() {
-    window.addEventListener("load", init);
   }
 }
